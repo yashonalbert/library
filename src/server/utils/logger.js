d1d441d97d933041035023c1fa10b1161dd3eec1
yashonalbert <814470240@qq.com>
2017-07-20 00:50:58 +0800

    Update log4js to add datefile

--- src/server/utils/logger.js
+++ src/server/utils/logger.js
@@ -1,9 +1,24 @@
 import log4js from 'log4js';
 import config from './config';
 
-log4js.loadAppender('file');
-log4js.addAppender(log4js.appenders.file(`${config.logs_dir}/koa.log`), 'koa');
-log4js.addAppender(log4js.appenders.file(`${config.logs_dir}/api.log`), 'api');
-log4js.addAppender(log4js.appenders.file(`${config.logs_dir}/schedule.log`), 'schedule');
+log4js.configure({
+  appenders: {
+    koa: {
+      type: 'dateFile',
+      filename: `${config.logs_dir}/koa.log`,
+    },
+    api: {
+      type: 'dateFile',
+      filename: `${config.logs_dir}/api.log`,
+    },
+    schedule: {
+      type: 'dateFile',
+      filename: `${config.logs_dir}/schedule.log`,
+    },
+  },
+  categories: {
+    default: { appenders: ['koa', 'api', 'schedule'], level: 'info' },
+  },
+});
 
 export default log4js.getLogger;
